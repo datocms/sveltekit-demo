17eca6245e5ffa9bdacd44b9efdead4828d39da3
Silvano Stralla <silvano.stralla@sistrall.it>
2023-03-17 15:04:46 +0100

    Force prerender on some pages

--- src/routes/posts/[slug]/+page.ts
+++ src/routes/posts/[slug]/+page.ts
@@ -1,5 +1,7 @@
 import type { RouteParams, PostContentVariables } from './$houdini';
 
+export const prerender = true;
+
 export const _PostContentVariables: PostContentVariables = ({
 	params: { slug }
 }: {
